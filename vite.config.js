16564a9ab2c8cdbf4a6cc2d009d65028a4042fd9
프라임 탐정 <polhun0304@naver.com>
2025-05-27 12:29:33 +0900

    빌드 성공 버전

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173
-  },
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html') // index.html 경로 지정
+    }
   }
-})
+});
